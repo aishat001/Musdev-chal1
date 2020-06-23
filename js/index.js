d069d4882169978c342476456e584abd51e8ceff
aishat001 <waheedaishat@gmail.com>
2020-06-23 22:37:57 +0100

    new files

---
+++ js/index.js
@@ -0,0 +1,5 @@
+
+document.querySelector(".toggle").addEventListener("click", function () {
+        document.querySelector(".links").classList.toggle("show-link");
+        document.querySelector(".new-icons").classList.toggle("show-icons");
+  });
